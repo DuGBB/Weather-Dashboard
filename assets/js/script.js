32abbbaaf128ecc0e61ba9c20748a7a18a953e05
Donald Biggers <DuGsEmail@yahoo.com>
2021-11-17 22:48:41 -0600

    api calls functionality input textarea and search button created linked apis

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,18 +9,18 @@ function weatherApi() {
 }
 }
 
-
 //weatherApi();
 
-function callWeatherApi() {
-    console.log("trying to use ajax")
+function callWeatherApi(apiChoice) {
     $(document).ready(function(){
         $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/weather?q=Boston",
+          
+           url: apiChoice, 
             type: "GET",
             success: function(result) {
                 console.log("api success");
                 console.log(result);
+                return result;
             },
             error: function(error){
                 console.log("api error");
@@ -30,4 +30,43 @@ function callWeatherApi() {
     });
 }
 
-callWeatherApi();
+function weatherDisplay() {
+    var weatherDisplay = document.getElementById("containerBlock");
+    var cityBox = document.createElement("textArea");
+    cityBox.setAttribute("id", "city");
+    var citySave = document.createElement("button");
+    citySave.innerHTML = "Search";
+    citySave.setAttribute("id", "citySearch");
+    citySave.addEventListener("click", function() {
+        citySearch();
+    });
+    weatherDisplay.appendChild(cityBox);
+    weatherDisplay.appendChild(citySave);
+}
+
+
+
+function localCheck(searchCity) {
+    console.log("BooYahh");
+    var localDictionary = localStorage.getItem("cityLongLad");
+    if (localDictionary === null) {
+        var callDictionary = {};
+    } else {
+        var callDictionary = JSON.parse(localDictionary);
+    }
+    console.log(callDictionary[searchCity]);
+    return callDictionary[searchCity];
+}
+
+function citySearch() {
+    var searchCity = document.getElementById("city").value;
+    console.log(searchCity);
+    var coords = localCheck(searchCity);
+    if (coords) {
+        var skipOne = callWeatherApi("https://api.openweathermap.org/data/2.5/onecall?lat=42.3584&lon=-71.0598&appid=");
+    } else {
+        var cityLatLong = callWeatherApi("https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=");
+    };
+}
+
+weatherDisplay();
