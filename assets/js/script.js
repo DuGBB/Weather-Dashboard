370b0fd7525d297f63a3141f8a5a03c5aa1e7601
Donald Biggers <DuGsEmail@yahoo.com>
2021-11-17 22:48:41 -0600

    api calls functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,8 +16,9 @@ function callWeatherApi() {
     console.log("trying to use ajax")
     $(document).ready(function(){
         $.ajax({
-            url: "https://api.openweathermap.org/data/2.5/weather?q=Boston",
-            type: "GET",
+            //url: "https://api.openweathermap.org/data/2.5/weather?q=Boston&appid=",
+            url: "https://api.openweathermap.org/data/2.5/onecall?lat=42.3584&lon=-71.0598&appid=",
+            ype: "GET",
             success: function(result) {
                 console.log("api success");
                 console.log(result);
